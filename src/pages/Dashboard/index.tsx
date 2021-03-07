16176fd58e148b25cbf211fb3ebce519d208e73c
djair <djhanlj@gmail.com>
2021-03-07 12:41:28 -0300

    criação dos estilo para o layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import { Title, Form, Repositories, Error } from './styled';
 import logoImg from '../../assets/logo.svg';
 
 interface Repository {
-  fullname: string;
+  full_name: string;
   description: string;
   owner: {
     login: string;
@@ -67,18 +68,20 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a key={repository.fullname} href="#">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
             />
             <div>
-              <strong>{repository.fullname}</strong>
+              <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+// import api from '../../services/api';
+import { Header, RepositoryInfo, Issue } from './styled';
+import logoImg from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+const Repository: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://xesque.rocketseat.dev/users/avatar/profile-d3cc11ac-faee-4da8-a7b9-924aec6801a2-1614858393544.jpg"
+            alt="Github explorer"
+          />
+          <div>
+            <strong>nome/repositorio</strong>
+            <p> teste teste teste teste teste</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issue>
+        <Link to="/repository/">
+          <div>
+            <strong>aaaaa</strong>
+            <p>teste teste</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issue>
+    </>
+  );
+};
 
 export default Repository;
